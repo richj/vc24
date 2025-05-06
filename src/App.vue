1424f7267cfd07678cc5cc3716271882753576df
richj <rich@unami.com>
2025-05-05 21:03:29 -0600

    jobs

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue';
+  import Navbar from '@/components/Navbar.vue';
+  import Hero from '@/components/hero.vue';
+  import HomeCards from '@/components/HomeCards.vue';
+  import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <Navbar />
+  <Hero title="Test Title" subtitle="test Subtitle"/>
+  <HomeCards />
+  <JobListings />
 </template>
---
+++ src/components/JobListings.vue
@@ -0,0 +1,12 @@
+<script setup>
+import jobData from "@/jobs.json";
+import { ref } from "vue";
+
+const jobs = ref(jobData);
+
+</script>
+<template>
+    <div v-for="job in jobs" :key="job.id">
+    {{  job.title }}
+    </div>
+</template>
